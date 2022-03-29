bdb1fd85cc7a7e880cca52ee6863ce661bf8649d
Alexandr Sanatar <allexvik@gmail.com>
2022-03-29 12:41:06 +0300

    0.1

---
+++ src/js/example.js
@@ -0,0 +1,14 @@
+//template
+import API from './ticketmasterAPI';
+const APIinstance = new API();
+async function TEST_API_FUNC() {
+    const test = await APIinstance.GetEvents();
+    console.log('test: ', test);
+    const testID = await APIinstance.GetEventsID();
+    console.log('testID: ', testID);
+    let changeID = APIinstance.eventID = 'vvG1YZpsud8PHH';
+    const testIDTWO = await APIinstance.GetEventsID();
+    console.log('testIDTWO: ', testIDTWO);
+    
+}
+TEST_API_FUNC();
---
+++ src/js/ticketmasterAPI.js
@@ -0,0 +1,36 @@
+export default class APIQuery {
+    constructor(){
+        this.API_KEY = 'RmnlUoo1ahrXxdghcRDQ1mLYffidaAYw';
+        this.ROOT_URL = 'https://app.ticketmaster.com/discovery/v2/';
+        this.EvID = 'Z7r9jZ1AdaeGo';
+    }
+    async GetEvents(){
+        try {
+            const projAPI = await fetch(`${this.ROOT_URL}events.json?apikey=${this.API_KEY}`);
+            const projAPI_JSONED = await projAPI.json();
+            return projAPI_JSONED;
+        }
+        catch (error){
+            console.log(error);
+        }
+
+    }
+    async GetEventsID(){
+        try {
+            const projAPI = await fetch(`${this.ROOT_URL}events/${this.EvID}.json?apikey=${this.API_KEY}`);
+            const projAPI_JSONED = await projAPI.json();
+            return projAPI_JSONED;
+        }
+        catch (error){
+            console.log(error);
+        }
+
+    }
+    get eventID(){
+        return this.EvID;
+    }
+    set eventID(newId){
+        this.EvID = newId;
+    }
+    
+}
