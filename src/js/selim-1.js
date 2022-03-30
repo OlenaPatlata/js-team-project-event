5a54a69fd5e889f3fc25cffb13a7ff2cdccdcfb3
Selim Fazylov <selim.fazylov1@gmail.com>
2022-03-30 15:28:25 +0300

    function

--- src/js/selim-1.js
+++ src/js/selim-1.js
@@ -8,11 +8,11 @@ const moreBtn = document.querySelector('.more-btn');
 moreBtn.addEventListener('click', MORE_BTN_FUNC);
 // заменить привязку к рабочему инпуту
 const valueInput = document.querySelector('.example__input');
-
+const authorList = document.querySelector('.author__list');
 // перед использованием функции необходимо изменить ключевое слово методом .eventKeyWord
 APIeventsByKey.eventKeyWord = 'sting';
 // это ОСНОВНАЯ ФУНКЦИЯ, заЭкспортить и подключить к нужной кнопке
-async function MORE_BTN_FUNC() {
+export default async function MORE_BTN_FUNC() {
     
     const result = await APIeventsByKey.GetEventsByKeyWord()
     console.log('testA:', result);
@@ -20,4 +20,38 @@ async function MORE_BTN_FUNC() {
     const {name, url, images, dates, _embedded } = resultArray[0];
     valueInput.value = name;
 
+    const markup = resultArray.map(({ name, url, images, dates, _embedded }) => {
+        return `<li class="author__item">
+      <div class="back-border"></div>
+      <div class="author__item-card" style="width: 180px">
+        <a href="${url}">
+          <picture>
+            <source
+              srcset="
+                ${images[5].url} 1x
+              "
+              media="(min-width:1200px)"
+            />
+            <source
+              srcset="
+                https://s1.ticketm.net/dam/a/da7/085e9630-9347-4af4-8762-3adb37925da7_1574381_CUSTOM.jpg 1x
+              "
+              media="(min-width:768px)"
+            />
+            <source
+              srcset="
+                https://s1.ticketm.net/dam/a/da7/085e9630-9347-4af4-8762-3adb37925da7_1574381_CUSTOM.jpg 1x
+              "
+              media="(min-width:320px)"
+            />
+            <img src="" alt="example" />
+          </picture>
+          <p class="name">${name}</p>
+          <p class="date">${dates.start.localDate}</p>
+          <p class="location">${_embedded.venues[0].city.name}</p>
+        </a>
+      </div>
+    </li>`
+    }).join("");
+    authorList.insertAdjacentHTML("beforebegin", markup);
 }
