09d56b01e287a9391d5ea41fd7c1b898eb4e0a33
Selim Fazylov <selim.fazylov1@gmail.com>
2022-03-30 16:13:23 +0300

    2.2 deleted conflicts

--- src/js/example.js
+++ src/js/example.js
@@ -1,24 +1,17 @@
 //template
 import API from './ticketmasterAPI';
 const APIinstance = new API();
-// -----------------------------
-import newAPIQuery from './newAPI';
-const newA = new newAPIQuery();
-newA.eventKeyWord = 'sting';
 
 async function TEST_API_FUNC() {
     const test = await APIinstance.GetEvents();
     console.log('test: ', test);
-    // const testID = await APIinstance.GetEventsID();
-    // console.log('testID: ', testID);
-    // let changeID = APIinstance.eventID = 'vvG1YZpsud8PHH';
-    // const testIDTWO = await APIinstance.GetEventsID();
-    // console.log('testIDTWO: ', testIDTWO);
-    // ---------------------------------------
-    const testA = await newA.GetEventsByKeyWord()
-    console.log('testA:', testA);
+    const testID = await APIinstance.GetEventsID();
+    console.log('testID: ', testID);
+    let changeID = APIinstance.eventID = 'vvG1YZpsud8PHH';
+    const testIDTWO = await APIinstance.GetEventsID();
+    console.log('testIDTWO: ', testIDTWO);
 }
-// TEST_API_FUNC();
+TEST_API_FUNC();
 
 
 
