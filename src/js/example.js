fd8fe038c1a53894c1dd7f8bf083c99ba4466608
Yevhen Bakhtizin <eugenbah@gmail.com>
2022-03-30 15:01:56 +0300

    1. Created an event card layout. 2. Created the markup and rendering of popular events.3. Added a spinner. 4.Added styles.

--- src/js/example.js
+++ src/js/example.js
@@ -1,14 +1,15 @@
 //template
 import API from './ticketmasterAPI';
 const APIinstance = new API();
+
 async function TEST_API_FUNC() {
-    const test = await APIinstance.GetEvents();
-    console.log('test: ', test);
-    const testID = await APIinstance.GetEventsID();
-    console.log('testID: ', testID);
-    let changeID = APIinstance.eventID = 'vvG1YZpsud8PHH';
-    const testIDTWO = await APIinstance.GetEventsID();
-    console.log('testIDTWO: ', testIDTWO);
-    
+  const test = await APIinstance.GetEvents();
+  console.log('test: ', test);
+  const testID = await APIinstance.GetEventsID();
+  console.log('testID: ', testID);
+  let changeID = (APIinstance.eventID = 'vvG1YZpsud8PHH');
+  const testIDTWO = await APIinstance.GetEventsID();
+  console.log('testIDTWO: ', testIDTWO);
 }
+
 TEST_API_FUNC();
---
+++ src/js/templates/eventCard.js
@@ -0,0 +1,37 @@
+const eventList = document.querySelector('.gallery');
+
+const eventCardMarkup = events =>
+  events
+    .map(
+      ({
+        images,
+        name,
+        dates: {
+          start: { localDate },
+        },
+        _embedded: {
+          venues: [{ name: place }],
+        },
+      }) => {
+        return `
+    <li class="gallery__item"> 
+        <img class="gallery__image" src=${images[0].url} alt=${name} />
+        <div class="gallery__wrapper">
+            <h3 class="gallery__subtitle">${name}</h3>
+            <p class="gallery__text">${localDate}</p>   
+            <p class="gallery__text gallery__text-location">
+              ${place}
+              </p>
+        </div>
+    </li>
+    `;
+      },
+    )
+    .join('');
+
+const renderMarkup = events => {
+  const markup = eventCardMarkup(events);
+  eventList.insertAdjacentHTML('beforeend', markup);
+};
+
+export { renderMarkup };
